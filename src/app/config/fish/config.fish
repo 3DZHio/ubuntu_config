7a39e2a0176d5be273504c334d02eb8d36786eae
3DZHio <3dzhio@gmail.com>
2024-09-22 21:07:59 +0200

    Update

--- src/app/config/fish/config.fish
+++ src/app/config/fish/config.fish
@@ -26,3 +26,9 @@ alias l="eza --oneline --long --header --git --color=always --icons=always"
 alias lt="eza --oneline --long --header --git --color=always --icons=always --tree"
 alias la="eza --oneline --long --header --git --color=always --icons=always --all"
 alias lat="eza --oneline --long --header --git --color=always --icons=always --all --tree"
+
+# ZOXIDE #
+alias cd="z"
+
+# FZF #
+alias f="fzf"
